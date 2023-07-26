29e212efc5d93035412f6df47b6baaea0ae2f9e4
hapiwangy <99786569+hapiwangy@users.noreply.github.com>
2023-07-26 14:50:12 +0800

    Create CutPdftoFiles.py

---
+++ CutPdftoFiles.py
@@ -0,0 +1,34 @@
+import PyPDF2
+import sys
+
+def cut_pages(filename="sample.pdf", endpoint="split_file", unit=0):
+    # 取得pdf頁數
+    reader = PyPDF2.PdfReader(filename)
+    # 不解密的話可能會報錯
+    if reader.is_encrypted:
+        reader.decrypt('')
+    page = len(reader.pages)
+    # 取得頁數的結尾
+    with open(filename, 'rb') as file:
+        filecount = 1
+        acc = 1
+        writer = PyPDF2.PdfWriter()
+        for x in range(page):
+            if acc != unit:
+                acc += 1
+                writer.add_page(reader.pages[x])
+            elif acc == unit:
+                writer.add_page(reader.pages[x])
+                with open(f"{endpoint}_{filecount}.pdf", 'wb') as output:
+                    writer.write(output)
+                acc = 1
+                writer = PyPDF2.PdfWriter()
+                filecount += 1
+        if acc != 1:
+            with open(f"{endpoint}_{filecount}.pdf", 'wb') as output:
+                writer.write(output)
+
+# 取得參數
+_, origin_file, endpoint_file, number_count = sys.argv
+cut_pages(origin_file, endpoint_file, int(number_count))
+# cut_pages("sample.pdf", "end", 2)
